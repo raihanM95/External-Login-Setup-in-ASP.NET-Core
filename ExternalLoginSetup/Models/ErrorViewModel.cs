5d187f6e22d920776bb88ceafc190fe9ed2fa5f4
Raihan Mahmud <mahmud.raihan@yahoo.com>
2020-03-03 11:20:50 +0600

    project init

---
+++ ExternalLoginSetup/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExternalLoginSetup.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
