f6e182fc1d42ed8a1ce711b83b3622fc7d0dfd90
Raihan Mahmud <mahmud.raihan@yahoo.com>
2020-03-03 17:51:53 +0600

    social logins

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# External-Login-Setup-in-ASP.NET-Core
+# ASP.NET Core 2.0 user authentication with social logins
+
+## Facebook
+```
+services.AddFacebook(options => {
+    options.AppId = "";
+    options.AppSecret = "";
+});
+```
+## Twitter
+```
+services.AddTwitter(options => {
+    options.ConsumerKey = "";
+    options.ConsumerSecret = "";
+})
+```
+## Github
+```
+services.AddGitHub(options => {
+    options.ClientId = "";
+    options.ClientSecret = "";
+})
+```
---
+++ Social.Logins.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace Social.Logins.Web.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Social.Logins.Web.Models;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() =>
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
